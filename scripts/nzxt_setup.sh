c4e396353d814b8552f30080604381ab4b03100d
= <=>
2024-04-26 21:55:55 -0500

    updates

---
+++ scripts/nzxt_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/ryandterri/git/liquidctl
+sudo python3 -m liquidctl set lcd screen liquid
+sleep 1
+sudo python3 -m liquidctl set lcd screen gif /home/ryandterri/Pictures/nzxt/tik-tok.gif
