65390a78072f5f79d24f129c93e31c2ff528d091
= <=>
2024-09-08 21:58:18 -0500

    updates

--- .config/hypr/scripts/lockscreentime.sh
+++ .config/hypr/scripts/lockscreentime.sh
@@ -1,18 +1,18 @@
 #/bin/bash
-#  ___    _ _      _   _                 
-# |_ _|__| | | ___| |_(_)_ __ ___   ___  
-#  | |/ _` | |/ _ \ __| | '_ ` _ \ / _ \ 
-#  | | (_| | |  __/ |_| | | | | | |  __/ 
-# |___\__,_|_|\___|\__|_|_| |_| |_|\___| 
-#                                        
-#  
-# by Stephan Raabe (2023) 
-# ----------------------------------------------------- 
+#  ___    _ _      _   _
+# |_ _|__| | | ___| |_(_)_ __ ___   ___
+#  | |/ _` | |/ _ \ __| | '_ ` _ \ / _ \
+#  | | (_| | |  __/ |_| | | | | | |  __/
+# |___\__,_|_|\___|\__|_|_| |_| |_|\___|
+#
+#
+# by Stephan Raabe (2023)
+# -----------------------------------------------------
 
 if [ -f "/usr/bin/swayidle" ]; then
-    echo "swayidle is installed."
-    #swayidle -w timeout 600 'swaylock -f' timeout 660 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
-    swayidle timeout 660 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
+  echo "swayidle is installed."
+  swayidle -w timeout 600 'swaylock -f' timeout 660 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
+  # swayidle timeout 660 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
 else
-    echo "swayidle not installed."
-fi;
+  echo "swayidle not installed."
+fi
